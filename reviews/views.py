b383228afb4cb308396ddda4279d6bdc41766d3a
Kate Brushkova <ekaterinabruskova7@gmail.com>
2021-03-24 14:46:45 +0200

    using var in template

--- reviews/views.py
+++ reviews/views.py
@@ -1,14 +1,6 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
 
 def index(request):
-    if request.GET.get('name'):
-        message = 'Your name: %r' % request.GET['name']
-    else:
-        message = 'Your name nothing!'
-    return HttpResponse(message)
-
-    # name = request.GET.get("name", "world")
-    # return HttpResponse("Hello, {}!".format(name))
+    name = 'Kate'
+    return render(request, 'reviews/base.html', {'name': name})
