bdf84c29d0d5aecc176b454058664b253a728b52
Kate Brushkova <ekaterinabruskova7@gmail.com>
2021-03-24 14:19:05 +0200

    create views

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    if request.GET.get('name'):
+        message = 'Your name: %r' % request.GET['name']
+    else:
+        message = 'Your name nothing!'
+    return HttpResponse(message)
+
+    # name = request.GET.get("name", "world")
+    # return HttpResponse("Hello, {}!".format(name))
