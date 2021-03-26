a1acce2f57796dae2ef28572909e2b67b082588c
Kate Brushkova <ekaterinabruskova7@gmail.com>
2021-03-26 10:55:41 +0200

    create a view function

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [path('', views.welcome_view, name='welcome_view')]
+urlpatterns = [
+    path('books/', views.book_list, name='book_list')
+]
