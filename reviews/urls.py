f204d9a6b905e6821e67900cc9c38deaa26d864f
Kate Brushkova <ekaterinabruskova7@gmail.com>
2021-03-26 13:55:04 +0200

    implement the book detail view

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('books/', views.book_list, name='book_list')
+    path('books/', views.books_list, name='books_list'),
+    path('books/<int:pk>/', views.books_detail, name='books_detail')
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
-from .models import Book, Review
+from django.shortcuts import render, get_object_or_404
+from .models import Book
 from .utils import average_rating
 
 
-def book_list(request):
-    books = Book.objects.all()
+def books_list(request):
+    books = Book.objects.filter(publication_date='2018-10-31')
     book_list = []
     for book in books:
         reviews = book.review_set.all()
@@ -21,3 +21,37 @@ def book_list(request):
         'book_list': book_list
     }
     return render(request, 'reviews/book_list.html', context)
+
+
+def books_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    reviews = book.review_set.all()
+    if reviews:
+        book_rating = average_rating([review.rating for review in reviews])
+        context = ({'book': book,
+                    'book_rating': book_rating,
+                    'reviews': reviews})
+
+    else:
+        context = ({'book': book,
+                    'book_rating': None,
+                    'reviews': None})
+
+    return render(request, 'reviews/book_detail.html', context)
+
+
+def books_publisher(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    reviews = book.review_set.all()
+    if reviews:
+        book_rating = average_rating([review.rating for review in reviews])
+        context = ({'book': book,
+                    'book_rating': book_rating,
+                    'reviews': reviews})
+
+    else:
+        context = ({'book': book,
+                    'book_rating': None,
+                    'reviews': None})
+
+    return render(request, 'reviews/book_detail.html', context)
