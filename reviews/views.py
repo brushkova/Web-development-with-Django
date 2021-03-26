cdaa50335c11c612c981db25be057c658ef7059e
Kate Brushkova <ekaterinabruskova7@gmail.com>
2021-03-26 15:43:49 +0200

    fixing the bug

--- reviews/views.py
+++ reviews/views.py
@@ -38,20 +38,3 @@ def books_detail(request, pk):
                     'reviews': None})
 
     return render(request, 'reviews/book_detail.html', context)
-
-
-def books_publisher(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    reviews = book.review_set.all()
-    if reviews:
-        book_rating = average_rating([review.rating for review in reviews])
-        context = ({'book': book,
-                    'book_rating': book_rating,
-                    'reviews': reviews})
-
-    else:
-        context = ({'book': book,
-                    'book_rating': None,
-                    'reviews': None})
-
-    return render(request, 'reviews/book_detail.html', context)
