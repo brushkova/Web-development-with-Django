1e609a7ac793f04f2547ec818a6af468b7d007f0
Kate Brushkova <ekaterinabruskova7@gmail.com>
2021-05-19 14:38:18 +0300

    create REST API

--- reviews/api_views.py
+++ reviews/api_views.py
@@ -1,9 +1,14 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .models import Book
+from rest_framework import generics
+
+from .models import Book, Contributor
+from .serializers import BookSerializer, ContributorSerializer
 
 
-@api_view()
-def first_api_view(request):
-    num_books = Book.objects.count()
-    return Response({"num_books": num_books})
+class AllBooks(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class ContributorView(generics.ListAPIView):
+    queryset = Contributor.objects.all()
+    serializer_class = ContributorSerializer
--- reviews/urls.py
+++ reviews/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views, api_views
 
 urlpatterns = [
-    path('api/first_api_view/', api_views.first_api_view),
-    path('', views.index),
+    path('api/contributors/', api_views.ContributorView.as_view(), name='contributors'),
+    path('api/all_books/', api_views.AllBooks.as_view(), name='all_books'),
     path('books/', views.book_list, name='book_list'),
     path('books/<int:pk>/', views.book_detail, name='book_detail'),
     path('books/book-search/', views.book_search, name='book_search'),
